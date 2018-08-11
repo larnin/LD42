eb17918161c64b28e4797d8286847d74e01dc7d7
Nicolas <nicolas-donald.laurent@laposte.net>
2018-08-11 22:50:54 +0200

    Loot config

--- Assets/Scripts/LootLogic.cs
+++ Assets/Scripts/LootLogic.cs
@@ -1,14 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 using Sirenix.OdinInspector;
+using NRand;
 
 public class LootLogic : SerializedMonoBehaviour
 {
     [SerializeField] ModifierBase m_modifier;
     [SerializeField] float m_rotationSpeed = 1;
+    [SerializeField] float m_baseModifierValue = 1;
+    [SerializeField] float m_minModifierLevel = 0.5f;
+    [SerializeField] float m_maxModifierLevel = 2;
 
     public ModifierBase modifier { get { return m_modifier; } }
-    
+
+    private void Awake()
+    {
+        var rand = new StaticRandomGenerator<DefaultRandomGenerator>();
+        var d = new UniformFloatDistribution(m_minModifierLevel, m_maxModifierLevel);
+
+        modifier.life = (int)((float)modifier.life * m_baseModifierValue + d.Next(rand) * GameInfos.level);
+        modifier.speed = (int)((float)modifier.speed * m_baseModifierValue + d.Next(rand) * GameInfos.level);
+        modifier.fireRate = (int)((float)modifier.fireRate * m_baseModifierValue + d.Next(rand) * GameInfos.level);
+        modifier.power = (int)((float)modifier.power * m_baseModifierValue + d.Next(rand) * GameInfos.level);
+    }
+
     void Update()
     {
         float angle = transform.rotation.eulerAngles.z + m_rotationSpeed * Time.deltaTime;
